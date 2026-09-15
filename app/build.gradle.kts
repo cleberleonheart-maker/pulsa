1f6a55dbdf7b7c68b7348e34b8d4e49929ebebd4
root <root@localhost.localdomain>
2026-09-15 19:26:39 +0000

    v3.58 (65): corrige versao (commit anterior nao bumpeou versionCode/Name)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.pulsa.player"
         minSdk = 23
         targetSdk = 34
-        versionCode = 64
-        versionName = "3.57"
+        versionCode = 65
+        versionName = "3.58"
     }
 
     buildFeatures {
